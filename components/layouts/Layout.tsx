e213f1a07323b3e84c6d098438fdc08e063ef87e
Oniel1721 <oni1san7alv21@gmail.com>
2022-05-22 12:56:28 -0400

    open graph meta tags added

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -3,13 +3,17 @@
 import Head from "next/head"
 
 import { FC } from "../../interfaces"
+import { isClientSide } from "../../utils";
 import { Navbar } from '../ui';
 
 interface Props {
   title?: string
 }
 
+const origin = isClientSide()?window.location.origin:''
+
 export const Layout: FC<Props> = ({ children, title }) => {
+  
   return (
    <>
     <Head>
@@ -17,6 +21,9 @@ export const Layout: FC<Props> = ({ children, title }) => {
         <meta name="author" content="Oniel Santos"/>
         <meta name="description" content={`Información sobre el pokémon ${title} `}/>
         <meta name="keywords" content={`${title}, pokemon, pokedex`}/>
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
     </Head>
 
     <Navbar />
