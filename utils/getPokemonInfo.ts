1b23bb9dc44039bd497b124da20b79406550a6e1
Oniel1721 <oni1san7alv21@gmail.com>
2022-05-22 13:42:03 -0400

    incremental static regeneration added

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,10 +2,15 @@ import { pokeApi } from "../api"
 import { PokemonFullResponse } from '../interfaces/pokemon-full';
 
 export const  getPokemonInfo = async (nameOrId: string)=>{
-    const { data } = await pokeApi.get<PokemonFullResponse>(`/pokemon/${nameOrId}`)
-    return { 
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites    
+    try{
+        const { data } = await pokeApi.get<PokemonFullResponse>(`/pokemon/${nameOrId}`)
+        return { 
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites    
+        }
+    }
+    catch(error){
+        return null;
     }
 }
